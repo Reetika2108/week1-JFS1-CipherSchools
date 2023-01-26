4aef009dbe0b9ddb87f73c5bcf85604bd52bdcc1
Reetika Sharma <reetikasharma2108@gmail.com>
2023-01-26 20:30:36 +0530

    Fourth commit

---
+++ Test3.java
@@ -0,0 +1,18 @@
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		char ch  = 'A';
+		while(ch<=90) {
+			System.out.print(ch+" ");
+			ch++;
+		}
+		System.out.println();
+		char ch1 = 'A';
+		do {
+			System.out.print(ch1+" ");
+			ch1++;
+		}while(ch1<='Z');
+	}
+
+}
